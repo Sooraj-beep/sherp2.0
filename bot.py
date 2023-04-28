55ea0a1953ade0a5c08fad1d6e7179045650bfdd
Andrew Li <zeyu.li23@gmail.com>
2023-04-28 14:20:34 -0600

    O(1) lookup time instead of O(n)

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import os
 
 SHERP_ID = "212613981465083906"
+SHERP_URL = "https://media.giphy.com/media/artj92V8o75VPL7AeQ/giphy.gif"
 
 # load the .env file
 load_dotenv()
@@ -16,17 +17,12 @@ with open("commands.json", "r") as f:
 
 @client.event
 async def on_message(message):
-    # find message.content in commands.json and append msg with the value
-    for command in commands:
-        if message.content == command:
-            msg = commands[command]
-
-            # if the message is from sherps, add a celebratory gif
-            if message.author.id == SHERP_ID:
-                msg = "https://media.giphy.com/media/artj92V8o75VPL7AeQ/giphy.gif " + msg
-
-            await message.channel.send(msg)
-
+    # stops the bot from responding to itself
+    if message.author.bot: return
+    
+    if message.content in commands:
+        response = commands[message.content]
+        await message.channel.send(SHERP_URL + response if message.auther.id == SHERP_ID else response)
 
 # run the bot using the token in the .env file
 client.run(os.getenv("BOT_TOKEN"))
