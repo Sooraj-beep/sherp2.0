0e4b0f0b6c7dd05021f54535af3cdc3e40ab90f6
Dhanrajbir Singh Hira <dhanrajb@ualberta.ca>
2023-08-10 18:16:27 -0600

    Change cog initialization to be concurrent.

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -1,14 +1,20 @@
 from .schedulebuddy import setup_schedule_buddy
 from .kattis import setup_kattis
-from .misc import setup_misc_cog 
-from .snipe import setup_snipe 
+from .misc import setup_misc_cog
+from .snipe import setup_snipe
 from .prereq import setup_prereq
 
+import asyncio
+from aiohttp import ClientSession
 
 
-async def setup_all_cogs(bot, guilds):
-    await setup_schedule_buddy(bot, guilds)
-    await setup_kattis(bot, guilds)
-    await setup_misc_cog(bot, guilds)
-    await setup_snipe(bot, guilds)
-    await setup_prereq(bot, guilds)
+async def setup_all_cogs(bot, guilds, client=None):
+    if not client:
+        client = ClientSession()
+    asyncio.gather(
+        setup_schedule_buddy(bot, guilds, client),
+        setup_kattis(bot, guilds),
+        setup_misc_cog(bot, guilds),
+        setup_snipe(bot, guilds, client),
+        setup_prereq(bot, guilds),
+    )
