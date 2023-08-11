715fa28ba768a055c42054115c412d8c5e3c425f
Dhanrajbir Singh Hira <dhanrajb@ualberta.ca>
2023-08-11 11:39:59 -0600

    Refactor 8ball into the misc cog.

--- cogs/misc.py
+++ cogs/misc.py
@@ -1,14 +1,23 @@
 import discord
 from discord.ext import commands
 
+import json
+import random
+
 
 class Misc(commands.Cog):
     def __init__(self, bot):
         self.__bot = bot
+        self.eight_ball_options = []
 
     async def cog_load(self):
         await super().cog_load()
         print("Misc(bbq23, java, sync) Cog loaded.")
+
+    async def load_data(self):
+        with open("data/8ball.json") as f:
+            data = json.load(f)
+            self.eight_ball_options = data["options"]
 
     @commands.command(name="bbq23")
     async def bbq23(self, ctx):
@@ -39,7 +48,12 @@ class Misc(commands.Cog):
         print("Sync complete")
         await ctx.send(f"Synced {len(commands)} commands")
 
+    @commands.command(name="8ball")
+    async def eight_ball(self):
+        return random.choice(self.eight_ball_options)
+
 
 async def setup_misc_cog(bot, guilds):
     cog = Misc(bot)
+    await cog.load_data()
     await bot.add_cog(cog, guilds=guilds)
