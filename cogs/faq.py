bce281bcf380c88646889a2f9e67e9636c59af64
Sooraj-beep <55115712+Sooraj-beep@users.noreply.github.com>
2023-09-10 13:05:43 -0600

    add prefix search and adding new faqs

--- cogs/faq.py
+++ cogs/faq.py
@@ -34,6 +34,50 @@ class Faq(commands.GroupCog, name="faq"):
             embed.add_field(name=header, value=row['Answer'], inline=False)
         await intr.response.send_message(embed=embed)
 
+    @app_commands.command(
+        name="search", description="search for a question with a prefix"
+    )
+    @app_commands.describe(prefix="Prefix to search for")
+    async def search(self, intr: discord.Interaction, prefix: str):
+        data = self.faq_sheet.get_all_records()
+        df = pd.DataFrame(data)
+
+        # make an embed with bolded question followed by (prefix: prefix) and in newline plain text the answer
+        embed = discord.Embed(title="Frequently Asked Questions", color=0x00ff00)
+        for index, row in df.iterrows():
+            if row['Prefix'] == prefix:
+                header = "**" + row['Question'] + "**" + " (prefix: " + row['Prefix'] + ")"
+                embed.add_field(name=header, value=row['Answer'], inline=False)
+        # if no questions with that prefix exist, send a message saying so
+        if len(embed.fields) == 0:
+            await intr.response.send_message("No questions with that prefix exist! Try using `/faq list` to see available prefixes.")
+        else:
+            await intr.response.send_message(embed=embed)
+    
+    # faq new <question> <answer> <prefix> [category]
+    @app_commands.command(
+        name="new", description="add a new question to the faq sheet"
+    )
+    @app_commands.describe(question="Question to add")
+    @app_commands.describe(answer="Answer to add")
+    @app_commands.describe(prefix="Prefix to add")
+    @app_commands.describe(category="Category to add")
+    async def new(self, intr: discord.Interaction, question: str, answer: str, prefix: str, category: str = None):
+        if category == None:
+            category = "General"
+
+        # dont add question if the prefix already exists
+        data = self.faq_sheet.get_all_records()
+        df = pd.DataFrame(data)
+        for index, row in df.iterrows():
+            if row['Prefix'] == prefix:
+                await intr.response.send_message("Prefix already exists!")
+                return
+
+        self.faq_sheet.append_row([question, answer, 1, category, prefix])
+        await intr.response.send_message("Added new question to faq sheet!")
+
+
 async def setup_faq(bot, guilds):
     cog = Faq()
     await cog.load_sheet()
