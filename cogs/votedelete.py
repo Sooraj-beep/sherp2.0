34f361e9092233fce7f9f344adc3d1df43f191cd
Utkarsh Yadav <36760455+kingslayer268@users.noreply.github.com>
2023-10-23 17:14:27 -0600

    Black formatting

--- cogs/votedelete.py
+++ cogs/votedelete.py
@@ -6,7 +6,13 @@ from helper import get_config
 
 __DEFAULT_EMOJI_STR = "<:ban:939740396286791741>"
 __DEFAULT_THRESHOLD = 5
-__DEFAULT_IGNORED_CHANNELS = [882731509553987717, 403057458299404289, 402891511991369742, 788501054077009981, 800858183799013376]
+__DEFAULT_IGNORED_CHANNELS = [
+    882731509553987717,
+    403057458299404289,
+    402891511991369742,
+    788501054077009981,
+    800858183799013376,
+]
 __cfg = get_config().get("votedelete", None)
 
 VOTEDELETE_EMOJI_STR = (
@@ -17,8 +23,11 @@ VOTEDELETE_THRESHOLD = (
     __cfg.get("threshold", __DEFAULT_THRESHOLD) if __cfg else __DEFAULT_THRESHOLD
 )
 IGNORED_CHANNELS = (
-    __cfg.get("ignored_channels", __DEFAULT_IGNORED_CHANNELS) if __cfg else __DEFAULT_IGNORED_CHANNELS
+    __cfg.get("ignored_channels", __DEFAULT_IGNORED_CHANNELS)
+    if __cfg
+    else __DEFAULT_IGNORED_CHANNELS
 )
+
 
 class VoteDelete(commands.Cog):
     def __init__(self, bot: discord.Client):
@@ -26,10 +35,11 @@ class VoteDelete(commands.Cog):
         self.threshold = VOTEDELETE_THRESHOLD
         self.ignored_channels = IGNORED_CHANNELS
         self.bot = bot
+
     async def cog_load(self):
         await super().cog_load()
         print("VoteDelete loaded.")
-    
+
     def checks(self, reaction):
         if str(reaction.emoji) != self.votedelete_emoji_str:
             return False
@@ -50,6 +60,7 @@ class VoteDelete(commands.Cog):
                 print("Permissions aren't configured correctly.")
             except discord.NotFound:
                 pass
+
 
 async def setup_votedelete(bot, guilds):
     await bot.add_cog(VoteDelete(bot), guilds=guilds)
