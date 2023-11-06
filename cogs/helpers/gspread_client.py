f91c0f2e4b30b707370a646b30250f0801a1691a
Sooraj-beep <55115712+Sooraj-beep@users.noreply.github.com>
2023-11-05 19:00:45 -0700

    install black and run it

--- cogs/helpers/gspread_client.py
+++ cogs/helpers/gspread_client.py
@@ -6,13 +6,19 @@ from helper import get_config
 __cfg = get_config().get("faq", {})
 GSHEET_URL = __cfg.get("gsheet_url")
 
+
 async def get_sheet():
     f = open("sherp-service-account-key.json", "r")
     service_account_key = json.load(f)
     f.close()
 
-    credentials = service_account.Credentials.from_service_account_info(service_account_key)
-    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+    credentials = service_account.Credentials.from_service_account_info(
+        service_account_key
+    )
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/drive",
+    ]
     creds_with_scope = credentials.with_scopes(scope)
     client = gspread.authorize(creds_with_scope)
 
